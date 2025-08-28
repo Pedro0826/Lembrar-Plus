88cf5e08ccf7cbc878adb2ac769fa9020000907d
Pedro0826 <pedrovitor0826@gmail.com>
2025-08-28 17:41:00 -0300

    Menu circular adicionado.

--- lib/screens/home_idoso.dart
+++ lib/screens/home_idoso.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../services/auth_service.dart';
+import 'package:circular_menu/circular_menu.dart';
 
 class HomeIdoso extends StatelessWidget {
   const HomeIdoso({super.key});
@@ -7,7 +9,31 @@ class HomeIdoso extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Página do Idoso")),
-      body: Center(child: Text("Bem-vindo, Idoso!")),
+      body: Stack(
+        children: [
+          const Center(child: Text("Bem-vindo, Idoso!")),
+          CircularMenu(
+            alignment: Alignment.bottomRight,
+            items: [
+              CircularMenuItem(
+                icon: Icons.logout,
+                color: Colors.grey,
+                onTap: () async {
+                  await AuthService().signOut();
+                  if (context.mounted) {
+                    Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
+                  }
+                },
+              ),
+              CircularMenuItem(
+                icon: Icons.info_outline,
+                color: Colors.red,
+                onTap: () {}, // item dummy
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
