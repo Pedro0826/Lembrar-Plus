bb93b018768df221adf53cbfe5524f9b5f12f025
Pedro0826 <pedrovitor0826@gmail.com>
2025-10-07 11:43:47 -0300

    Implementação de notificações dos medicamentos.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true // Habilita o core library desugaring
     }
 
     kotlinOptions {
@@ -45,4 +46,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Adiciona a dependência para core library desugaring
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 // lib/main.dart
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:timezone/data/latest.dart' as tz;
 import 'package:flutter/material.dart';
 import 'app.dart'; // você cria esse arquivo
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  tz.initializeTimeZones();
   runApp(const App());
 }
