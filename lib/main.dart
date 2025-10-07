9fcfa9dd6d09d9be03f056b4d05d9b8f98e76097
Pedro0826 <pedrovitor0826@gmail.com>
2025-10-07 16:42:42 -0300

    Corrigido implementação das notificações.

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'app.dart'; // você cria esse arquivo
+import 'services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseMessaging.instance.requestPermission();
+  await ServicoNotificacao.inicializar(); // Inicializa timezone e notificações locais
   runApp(const App());
 }
